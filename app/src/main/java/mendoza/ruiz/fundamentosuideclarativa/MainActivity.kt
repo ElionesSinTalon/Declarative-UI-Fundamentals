3d0a6d99a5f50d15f36f8feb2398e11d3f660c52
ElionesSinTalon <pedroagresivo2.0@gmail.com>
2026-02-22 10:24:59 -0600

    Updating the application

--- app/src/main/java/mendoza/ruiz/fundamentosuideclarativa/MainActivity.kt
+++ app/src/main/java/mendoza/ruiz/fundamentosuideclarativa/MainActivity.kt
@@ -1,25 +1,38 @@
 package mendoza.ruiz.fundamentosuideclarativa
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
@@ -28,19 +41,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import mendoza.ruiz.fundamentosuideclarativa.ui.theme.FundamentosUIdeclarativaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
        enableEdgeToEdge()
         setContent {
-            MainScreen()
+            MaterialTheme{
+                MainScreen()
+            }
         }
     }
 }
+    @OptIn(ExperimentalMaterial3Api::class)
 @Composable      //MI PRIMERA FUNCION
 fun MainScreen(){
     var counter by remember {mutableStateOf(0)}
@@ -48,28 +64,40 @@ fun MainScreen(){
     val tasks = remember {
         mutableStateListOf(
             "Aprender compose",
-            "Sacar la basura",
+            "Preparar la cena",
             "Ir de compras",
             "Subir proyecto a GitHub"
         )
     }
    Scaffold(
        topBar = {
-               //tittle = {Text("TaskCounterApp")}
+           TopAppBar(
+               title = {Text("Lista de tareas")}
+           )
        }
-   ) { paddingValues ->
+    ) { paddingValues ->
        Column(
            modifier = Modifier
                .padding(paddingValues)
-               .fillMaxWidth()
+               .fillMaxSize()
+
        ) {
            CounterHeader(
-               counter = counter,
-               onIncrement = {counter++}
+               counter = tasks.size,
+               onAddTask = {
+                   nombreDeLaTarea ->
+                   if(!tasks.contains(nombreDeLaTarea)){
+                       tasks.add(nombreDeLaTarea)
+                   }
+               }
            )
-           TaskList(tasks = tasks)
+           TaskList(
+               tasks = tasks,
+               onDeleteTask = {tareaBorrar ->
+                   tasks.remove(tareaBorrar)// <- Eliminamos la tarea de la lista
+               }
+           )
        }
-
    }
 }
 
@@ -77,7 +105,10 @@ fun MainScreen(){
 @Composable  //MI SEGUNDA FUNCION
 fun CounterHeader(
     counter: Int,
-    onIncrement:() -> Unit){
+    onAddTask: (String) -> Unit
+){
+    var textInput by remember {mutableStateOf("")}
+
     Card(
         modifier = Modifier
             .padding(16.dp)
@@ -85,67 +116,131 @@ fun CounterHeader(
         elevation = CardDefaults.cardElevation(6.dp)
     ){
         Column(
-            modifier = Modifier.padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ){
+            modifier = Modifier
+                .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+        ) {
             Text(
-                text = "Tareas por cumplir",
+                text = "Tareas por cumplir: $counter",
                 style = MaterialTheme.typography.titleMedium
-
             )
 
+            //Campo de texto para que el usuario escriba
+            OutlinedTextField(
+                value = textInput,
+                onValueChange = {nuevoTexto -> textInput = nuevoTexto},
+                label = {Text("Nombre de la nueva tarea")},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp)
+            )
             Button(
-                onClick = onIncrement,
+                onClick = {
+                    if (textInput.isNotBlank()){
+                        onAddTask(textInput)
+                        textInput = ""
+                    }
+                },
+                //La linea 146 es una forma para cambiar el color de fondo de un boton
+                colors = ButtonDefaults.buttonColors( Color.DarkGray),
                 modifier = Modifier
                     .align(Alignment.CenterHorizontally)
                     .padding(top = 8.dp)
 
-            ){
+            ) {
                 Text("Agregar tarea")
             }
         }
     }
-}
-//Fin
-@Composable   //MI TERCERA FUNCION
-fun TaskItem(task: String){
-    var expanded by remember { mutableStateOf(false) }
 
-    Card(
-        modifier = Modifier
-            .padding(horizontal = 16.dp, vertical = 8.dp)
-            .fillMaxWidth()
-            .clickable{expanded = !expanded}
-    ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text(
-                text = task,
-                style = MaterialTheme.typography.titleMedium
+}
+
+//Fin
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable   //MI TERCERA FUNCION
+fun TaskItem(
+    task: String,
+    onDelete: () -> Unit
+){
+    var expanded by remember {mutableStateOf(false)}
+
+    //Control del estado (deslizamiento)
+    val dismissState = rememberSwipeToDismissBoxState(
+        confirmValueChange = {dismissValue ->
+          if (dismissValue == SwipeToDismissBoxValue.EndToStart){
+              onDelete()
+              true
+          }else {
+              false
+          }
+        }
+    )
+    SwipeToDismissBox(
+        state = dismissState,
+        enableDismissFromStartToEnd = false,
+        backgroundContent = {
+            //Fondo rojo que aparece al deslizar
+            val color by animateColorAsState(
+                if (dismissState.targetValue == SwipeToDismissBoxValue.EndToStart)
+                Color.Red else Color.Transparent,
+                label = "ColorAnimacion"
             )
 
-            AnimatedVisibility(visible = expanded) {
-                Text(
-                    text = "Toca para marcar como completado",
-                    style = MaterialTheme.typography.bodyMedium,
-                    modifier = Modifier.padding(top = 8.dp)
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .padding(end = 16.dp),
+                contentAlignment = Alignment.CenterEnd){
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Eliminar tarea",
+                    tint = Color.White
                 )
+            }
+        }
+    ){
+        Card(
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .fillMaxWidth()
+                .clickable{expanded = !expanded}
+        ){
+            Column(modifier = Modifier.padding(16.dp)){
+                Text(
+                    text = task,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                AnimatedVisibility(visible = expanded) {
+                    Text(
+                        text = "Marcar como compeltado",
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
+                }
             }
         }
     }
 }
 
 @Composable  //MI CUARTA FUNCION
-fun TaskList(tasks: List<String>){
-    LazyColumn(modifier = Modifier.fillMaxSize()
-    ) {
-        items(tasks) { task ->
-            TaskItem(task = task)
+fun TaskList(tasks: List<String>, onDeleteTask: (String) -> Unit){
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(
+            items = tasks,
+            key = { it }
+        ){
+            task ->
+            TaskItem(
+                task = task,
+                onDelete = {onDeleteTask(task)}
+            )
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun MainScreenPreview(){
     MainScreen()
