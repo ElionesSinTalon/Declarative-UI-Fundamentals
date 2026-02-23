abc2359480083892f63f47d0f3cf65c8f1806d9c
ElionesSinTalon <pedroagresivo2.0@gmail.com>
2026-02-23 10:01:02 -0600

    Updating the repository

--- app/src/main/java/mendoza/ruiz/fundamentosuideclarativa/MainActivity.kt
+++ app/src/main/java/mendoza/ruiz/fundamentosuideclarativa/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
